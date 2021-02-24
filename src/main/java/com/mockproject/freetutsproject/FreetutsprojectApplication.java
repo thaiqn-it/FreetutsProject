dc8be98c3448c799f0b5a0d6e9d32d4a96566f04
thaiqn-it <qthai20102000@gmail.com>
2021-02-24 14:58:08 +0700

    first update

---
+++ src/main/java/com/mockproject/freetutsproject/FreetutsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.mockproject.freetutsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreetutsprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreetutsprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mockproject/freetutsproject/FreetutsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockproject.freetutsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreetutsprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
