e0b2e7bfb11eccabcf58a9b868e7cd1bdee97e40
thaiqn-it <qthai20102000@gmail.com>
2021-04-02 14:29:49 +0700

    update edit category

--- src/main/java/com/mockproject/freetutsproject/repository/CategoryRepository.java
+++ src/main/java/com/mockproject/freetutsproject/repository/CategoryRepository.java
@@ -1,5 +1,6 @@
 package com.mockproject.freetutsproject.repository;
 
+import com.mockproject.freetutsproject.dto.CategoryDTO;
 import com.mockproject.freetutsproject.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,5 @@ public interface CategoryRepository extends JpaRepository<CategoryEntity, Long>{
 	List<CategoryEntity> findBySubCategoriesIsNullAndAvailableTrue();
 	CategoryEntity findByIdAndAvailableTrue(Long id);
 	List<CategoryEntity> findByParentId(long id);
+
 }
